6df7de1d7b4d3eff3bd95bee37061957779306d1
jinjin <jinjinx28@gmail.com>
2026-03-18 12:31:36 +0900

    2026-03-18 commit

---
+++ javascript/hello.js
@@ -0,0 +1,12 @@
+// hello.js > 자바스크립트 엔진(Node.js)에서 바로 실행
+
+let a = 100;
+let b = 200;
+let sum = a + b;
+console.log('a = ', a);
+console.log ('b = ', b);
+console.log('sum = ', sum);
+console.log('a + b = ', (a + b));
+
+let name ='홍길동'
+console.log(naem);
