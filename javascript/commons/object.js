20c8b75169159b89e3470bdb8d7a961aaf407b6e
jinjin <jinjinx28@gmail.com>
2026-03-24 15:43:06 +0900

    2026-03-24 commit

--- javascript/commons/object.js
+++ javascript/commons/object.js
@@ -1,18 +1,35 @@
-/*
-    객체 CRUD 관리 모듈
-*/
-/*
-    객체에 값을 추가하는 함수 정의, 모든 객체에서 사용할 수 있도록 일반화 시킴
-    C (Create)
-*/
+
+// 객체 생성 모듈
+
+let objects = {};   // {hong : {}, kim : {}}
+
+export function getObjects() {  //전체 리스트
+    return objects;
+}
+
+export function createObject(objectName) {  //객체 생성
+    return objects[objectName] = {};
+}
+
+export function deleteObject(objectName) {  //객체 삭제
+    delete objects[objectName];
+}
+
+export function getObject(objectName) {
+    return objects[objectName];
+}
+
+//객체 CRUD 관리 모듈
+/* 객체에 값을 추가하는 함수 정의, 모든 객체에서 사용할 수 있도록 일반화 시킴
+    C (Create) */
 export function setValue(object, key, value) {
     object[key] = value;
 }
 
 // 2-1. 객체 값 출력
-export function getValue(object, key) {
-    console.log(object[key]);
-}
+// export function getValue(object, key) {
+//     console.log(object[key]);
+// }
 
 // 2-2. 객체 값 반환 - R (Read)
 export function getValue(object, key) {
