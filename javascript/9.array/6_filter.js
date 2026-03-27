ae087355004b718883b37eb2b00d7efa2adfb196
jinjin <jinjinx28@gmail.com>
2026-03-27 11:42:51 +0900

    2026-03-27 commit

--- javascript/9.array/6_filter.js
+++ javascript/9.array/6_filter.js
@@ -1,3 +1,33 @@
 /*
     배열 객체에서 필터링을 진행한 후 Shallow Copy(얕은 복사) 진행 후 리턴
+    - 배열객체.filter(callback)
 */
+
+let strList = ['jinjin', 'nyong', 'nuri', 'friren'];
+
+//이름이 6자 이상인 사람 필터링
+let filterObj = strList.filter((str)=> str.length > 5);
+console.log(`filterObj = ${filterObj}`);
+
+let fruits = [
+    {name : 'strawberry', emoji :'🍓' },
+    {name : 'lemon', emoji :'🍋' },
+    {name : 'coconut', emoji :'🥥' },
+    {name : 'strawberry', emoji :'🍓' },
+    {name : 'lemon', emoji :'🍋' },
+    {name : 'coconut', emoji :'🥥' }
+];
+
+//strList에서 4자 이상인 이름만 필터링
+let filterObject = strList.filter((str)=> str.length > 4);
+console.log(`filterObj = ${filterObj}`)
+
+//fruits 객체에서 lemon 객체를 출력
+let lemon = fruits.filter((fruit) => fruit.name === 'lemon');
+let lemonFind = fruits.find((fruit) => fruit.name === 'lemon');
+console.log();
+console.log(lemon);
+console.log(lemonFind);
+console.log(lemon[0].name, lemon[0].emoji);
+console.log(lemonFind.name, lemonFind.emoji);
+
---
+++ javascript/9.array/7_map.js
@@ -0,0 +1,17 @@
+/*
+    배열의 모든 요소를 순회하며, 콜백함수의 조건을 적용하여 새로운 배열 생성 후 반환
+    - 배열객체.map(callback)
+*/
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let fnumbers = [1.123, 2.345, 3.2743]
+
+//numbers의 모든 요소에 x100 한 결과를 출력
+
+let result1 = numbers.map((number) => number*100);
+console.log(result1);
+
+//fnumbers의 모든 요소를 정수로 반환
+let result2 = fnumbers.map(fnumber => Math.floor(fnumber))
+console.log(result2);
+
