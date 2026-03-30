24e5d47a2ff74f136ee7b97f95505227cb70c337
jinjin <jinjinx28@gmail.com>
2026-03-30 10:49:34 +0900

    2026-03-30 commit

---
+++ javascript/10.iterable/5_rest_parameter.js
@@ -0,0 +1,27 @@
+/*
+    Rest Parameter(나머지 인자) : 파라미터 인자를 배열로 저장함
+    - 함수의 매개변수에 정의함
+    - function 함수명 (...Rest Parameter) {}
+    - const 함수명 (...Rest Parameter) => {}
+*/
+
+// function add(a, b, ...numbers) {
+//     let sum = numbers.reduce((acc, cur) => acc + cur);
+//     console.log(a, b, numbers, sum);    
+//     return a + b + sum;
+// }
+
+function add (a, b, ...numbers) {
+    return a + b + numbers.reduce((acc, cur) => acc + cur);
+}
+  
+const fadd = (f1, ...fruits) => {      //...fruits : rest 파라미터
+    console.log(f1, fruits);
+}
+
+let result1 = add(1, 2, 3, 4, 5);
+let result2 = add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(result1, result2);
+
+let fruits = ['🍓', '🍉', '🥥', '🥝'];
+fadd('🍋', ...fruits);  //...fruits : spread 연산자 ( 전개 구문 )
