8ce36ac25904a1f2bc0bc2cb7315c81f13ffd169
jinjin <jinjinx28@gmail.com>
2026-04-01 15:12:03 +0900

    2026-04-01 commit

--- javascript/14.json/2_kobis.js
+++ javascript/14.json/2_kobis.js
@@ -1,17 +1,63 @@
-let key = `0b9b6c35fe9f88366306e9356d6668e1`;
-let targetDt = '20260331';
-const data_url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDt}`;
+let key = `6206e4e96646c240ac59f09bc0164ff9`;
+
+const getJson = async(type, targetDt) => {
+    const data_url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/search${type}BoxOfficeList.json?key=${key}&targetDt=${targetDt}`;
+    let response = await fetch(data_url);
+    return response.json();
+}
+
 
 //handleBoxOffice 함수 정의
-const handleBoxOffice = () => {
-    let sdate = document.querySelector('#sdate').value;
-    if(sdate === '') {
-        alert('날짜를 선택해주세요.');
+const handleBoxOffice = async() => {
+    let type = document.querySelector('#type').value;
+    let sdate = document.querySelector('#sdate').value;  
+    
+    if(type === 'default') {
+        alert('타입을 선택해주세요');
+        document.querySelector('#type').focus();        
+    } else if(sdate === '') {
+        alert('날짜를 선택해주세요');
         document.querySelector('#sdate').focus();
     } else {
-        let dateArray = new String(sdate).split("-");
-        targetDt = dateArray[0] + dateArray[1] + dateArray[2]
-        console.log(sdate);   
-        console.log(targetDt);   
-    }
+        targetDt = sdate.split("-").reduce((acc, cur)=> acc+cur);
+        let kobis = await getJson(type, targetDt); 
+        let kobisBoxOffice =  kobis.boxOfficeResult;
+        let kobisBoxOfficeList = null;
+        if(type === 'Daily') {
+            kobisBoxOfficeList =  kobis.boxOfficeResult.dailyBoxOfficeList;
+        } else {
+            kobisBoxOfficeList =  kobis.boxOfficeResult.weeklyBoxOfficeList;
+        }
+console.log(kobis);
+
+        let output = `
+            <h1>${kobisBoxOffice.boxofficeType}</h1>
+            <h3>${kobisBoxOffice.showRange}</h3>
+            <table border=1>
+                <tr>
+                    <th>순위</th>
+                    <th>영화제목</th>
+                    <th>개봉일</th>
+                    <th>당일관객수</th>
+                    <th>누적관객수</th>
+                    <th>누적매출액</th>
+                </tr>
+                ${
+                    kobisBoxOfficeList.map((movie) => `
+                        <tr>
+                            <td>${movie.rank}</td>
+                            <td>${movie.movieNm}</td>
+                            <td>${movie.openDt}</td>
+                            <td>${parseInt(movie.audiCnt).toLocaleString()}</td>
+                            <td>${parseInt(movie.audiAcc).toLocaleString()}</td>
+                            <td>${parseInt(movie.salesAcc).toLocaleString()}</td>
+                        </tr>
+                    `).join("")
+                }
+            </table>
+        `;
+
+        document.querySelector('#content').innerHTML = output;
+    } 
+    
 }
