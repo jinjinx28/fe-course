4d14a8fc24a5b6a16aa02348994d4b78174c73ec
jinjin <jinjinx28@gmail.com>
2026-04-06 12:46:02 +0900

    2026-04-06 commit

--- cgv-demo/js/boxoffice.js
+++ cgv-demo/js/boxoffice.js
@@ -5,7 +5,19 @@ const kmdb_key = `VEH21W26T702JJEV1F44`;
 
 /* KOBIS API - 박스오피스 가져오기 */
 async function getKobis() {
-    let url = `	http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${kobis_key}&targetDt=20260405`;
+    let date = new Date();
+    let year = new String(date.getFullYear());
+    let month = new String(date.getMonth()+1);
+    let day = new String(date.getDate()-1);
+    month = (month.length<2)? '0'.concat(month) : month;
+    day = (day.length<2)? '0'.concat(day) : day;
+
+    let today = year.concat(month).concat(day);
+
+    // console.log(month.length);
+    console.log(today);
+    
+    let url = `	http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${kobis_key}&targetDt=${today}`;
     console.log(url);
 
     let response = await fetch(url);
@@ -30,10 +42,46 @@ async function getPoster(movieNm, openDt) {
     return posters?.[0];
 }
 
-async function createBocoffice() {
+ async function showList(list) {
+    let slist = list.slice(0, 5).map( async (movie, idx) => {
+        let movieNm = movie.movieNm;
+        let openDt = movie.openDt;
+        let audiAcc = movie.audiAcc;
+        let poster = await getPoster(movie.movieNm, movie.openDt);
+        // console.log(movie.movieNm, movie.openDt, poster);
+        return {movieNm, openDt, audiAcc, poster}
+    });  
+
+    return await Promise.all(slist);
+}
+
+async function createBoxoffice() {
     let kobis = await getKobis();
     let list = kobis.boxOfficeResult.dailyBoxOfficeList;
-    console.log(kobis);
+    let outputList = await showList(list);
+    // console.log('showList =>', outputList);
+
+    let output = `
+        <ul>
+            ${
+                outputList.map((movie) => `
+                    <li>
+                        <div>
+                            <img src="${movie.poster}" 
+                                alt="moviechart img"
+                                width="200px">
+                        </div>
+                        <div><h4>${movie.movieNm}</h4></div>
+                        <div><h5>누적관객수${parseInt(movie.audiAcc).toLocaleString()}</h5></div>
+                    </li>
+                `).join("")
+            }
+        </ul>
+    `;
+    console.log(output);
+    document.querySelector('#boxoffice').innerHTML = output;
+    
+    
     // let showList = [];
 
     // let idx = 0;
@@ -44,21 +92,10 @@ async function createBocoffice() {
     //         break;
     //     }
     // }
-
-    let showList = list.slice(0, 5).map( async (movie, idx) => {
-        let movieNm = movie.movieNm;
-        let openDt = movie.openDt;
-        let poster = await getPoster(movie.movieNm, movie.openDt);
-        // console.log(movie.movieNm, movie.openDt, poster);
-        return {movieNm, openDt, poster}
-    });   //[{movieNm : 영화제목, openDt : 개봉일 , poster : 포스터}, {~}...]
-
-    console.log('showList =>', showList);
-    
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    createBocoffice();
+    createBoxoffice();
 });
 
 
