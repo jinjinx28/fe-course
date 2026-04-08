67bf5e4132c9f265d3fa8abb06322e0f5049dbdf
jinjin <jinjinx28@gmail.com>
2026-04-08 12:24:36 +0900

    2026-04-08 commit

--- basic-app/src/components/Parent.jsx
+++ basic-app/src/components/Parent.jsx
@@ -1,14 +1,18 @@
 import Child from "./Child.jsx"
 
+function PropsComp() {
+    return (
+        <h1>PropsComp!!</h1>
+    )
+}
 export default function Parent(props) {
-    let info = {
-        name : "진진",
-        age : "23"
-    }
+
     return (
         <>
             <h1>Parent :: {props.name}</h1>
-            <Child name="jinjin" age = "23" info = {info}/>
+            <Child>
+                <PropsComp/>
+            </Child>     
         </>
     )
 }
