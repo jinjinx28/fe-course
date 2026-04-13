8fa35fd7a7feffce85395bebdba2a232bbfe7317
jinjin <jinjinx28@gmail.com>
2026-04-13 15:29:48 +0900

    2026-04-13 commit

---
+++ basic-app/src/AppEffect.jsx
@@ -0,0 +1,9 @@
+import EffectCounter from '../src/components/effects/EffectCounter.jsx';
+
+export default function App() {
+    return(
+        <>
+            <EffectCounter/>
+        </>
+    )
+}
--- basic-app/src/main.jsx
+++ basic-app/src/main.jsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client'
 // import App from './AppButton.jsx'
 // import App from './AppCommon.jsx'
 // import App from './AppCounter.jsx'
-import App from './App.jsx'
+import App from './AppEffect.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
