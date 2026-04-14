fbeac1ebc0d1dedeb2eedb57cb527c87166b57c2
jinjin <jinjinx28@gmail.com>
2026-04-14 17:20:47 +0900

    2026-04-14 commit

--- portfolio-react-app/src/App.jsx
+++ portfolio-react-app/src/App.jsx
@@ -1,15 +1,15 @@
-import Header from './components/Header.jsx';
-import Content from './components/Component.jsx';
-import Footer from './components/Footer.jsx';
 import './App.css';
-import { fetchData } from './util/fetch.js';
 import { useState, useEffect } from 'react';
+import { fetchData } from './util/fetch.js';
+import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
   const [data, setData] = useState({});
-  useEffect(() => {
-    const loadData = async () => {
-      const jsonData = await fetchData("http://localhost:5174/data/portfolio.json");
+  useEffect(()=>{
+    const loadData = async() => {
+      const jsonData = await fetchData("http://localhost:5173/data/portfolio.json");
       setData(jsonData);
     }
     loadData();
@@ -17,10 +17,9 @@ export default function App() {
 
   return (
     <>
-      <Header data = {data?.header}/>
+      <Header data={data?.header} />
       <Content />
       <Footer />
     </>
   )
 }
-
--- portfolio-react-app/src/components/Header.jsx
+++ portfolio-react-app/src/components/Header.jsx
@@ -1,22 +1,20 @@
-import Logo from './header/Logo.jsx';
-import MenuList from './commons/MenuList.jsx';
-import ToggleButton from './header/ToggleButton.jsx';
+import Logo from "./header/Logo.jsx";
+import ToggleButton from "./header/ToggleButton.jsx";
+import MenuList from "./commons/MenuList.jsx";
 
-export default function Header({data}) {
-    //data 객체가 존재하는 경우에만 구조분해할당 진행!!
-    // 구조분해 할당으로 반환하는 타입이 객체인 경우
+export default function Header({ data }) {
+    //✨data 객체가 존재하는 경우에만 구조분해할당 진행! 
+    //✨구조분해 할당으로 반환하는 타입이 객체인 경우에는 초기화 작업 필수
+    const { menus = [] } = data || {};  
 
-    const [menus] = data || {};
-    
-    return(
+    return (
         <header class="header">
-            <Logo img="img/favicon.ico"
-                alt="header-logo"
-                style="header-logo-img"
-                title="jinjin"
+            <Logo   img="/images/favicon.ico"
+                    alt="header-logo"
+                    style="header-logo-img"
+                    title="Judy"
             />
-            
-            <MenuList menus={menus} style="header-menu-open"/>
+            <MenuList menus={menus} style="header-menu open" />
             <ToggleButton />
         </header>
     )
